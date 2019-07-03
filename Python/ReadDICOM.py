138790dad323a9aa0196c318337a5442c1931b16
Giorgio Morales <giorgiomoralesluna@gmail.com>
2019-07-03 17:08:06 -0500

    giorgio

--- Python/ReadDICOM.py
+++ Python/ReadDICOM.py
@@ -24,14 +24,14 @@ inputdir = 'E://Giorgio//Kagle//Train/'
 maskdir = 'E://Giorgio//Kagle//Masks/'
 
 # Verifica que todas las imágenes tengan máscara, sino las mueve
-orig_path = 'E://Giorgio//Kagle//Train/*.dcm'
-for dir in glob.glob(orig_path):
-    mdir = os.path.basename(dir)[:-4] + ".tif"
+# orig_path = 'E://Giorgio//Kagle//Train/*.dcm'
+# for dir in glob.glob(orig_path):
+#     mdir = os.path.basename(dir)[:-4] + ".tif"
+#
+#     if not os.path.exists(os.path.join(maskdir, mdir)):
+#         move(dir, 'E://Giorgio//Kagle//Unused//' + os.path.basename(dir))
 
-    if not os.path.exists(os.path.join(maskdir, mdir)):
-        move(dir, 'E://Giorgio//Kagle//Unused//' + os.path.basename(dir))
-
-d = '1.2.276.0.7230010.3.1.4.8323329.32767.1517875162.258059'
+d = '1.2.276.0.7230010.3.1.4.8323329.352.1517875162.525205'
 ds = pydicom.read_file(os.path.join(inputdir, d) + '.dcm')
 img = ds.pixel_array
 
