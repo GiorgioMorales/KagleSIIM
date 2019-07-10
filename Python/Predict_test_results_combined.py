a3f07b06ab1c5029d401d651eddc2204fecd9f80
DIDT03 <giorgiomoralesluna@gmail.com>
2019-07-10 09:13:20 -0500

    commit para clasificacion

---
+++ Python/Predict_test_results_combined.py
@@ -0,0 +1,137 @@
+from keras.models import Model, load_model
+from models_Giorgio import compiled_model, focal_loss
+from keras.optimizers import Adam
+import pydicom
+import cv2
+import os
+import glob
+import numpy as np
+import time
+
+import csv
+from mask_functions import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+K.set_learning_phase(0)
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Carga modelo
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+dim = 256
+model = compiled_model('build_clasificator', dim=dim, n_channels = 1, lr = 0.0003, loss = 'focal_loss')
+model.load_weights('weights-trainclass-100-0.8078.h5')
+optimizer = Adam(lr=0.03, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+model.compile(optimizer=optimizer, loss=focal_loss, metrics=['acc'])
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Carga imágenes test
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+basepath = os.getcwd()[:-7]
+orig_path = basepath + '//Test//*.dcm'
+
+# Obtiene una lista de las direcciones de las imágenes y sus máscaras
+addri = sorted(glob.glob(orig_path))
+
+# Define directorio de máscaras de test
+maskpath = basepath + '//MasksTest//'
+
+dim = 256
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Carga imágenes 
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+ids = []
+
+images = np.zeros((len(addri), dim, dim, 1), dtype=np.uint8)
+
+for cnt, dir in enumerate(addri):
+
+    # Lee imagen
+    ds = pydicom.read_file(dir)
+    img = ds.pixel_array
+
+    if dim != 1024:
+        img = cv2.resize(img, (dim, dim))
+
+    images[cnt,] = np.reshape(img, (dim, dim, 1))
+
+    # Extrar nombre
+    name = os.path.basename(dir)[:-4]
+    ids.append(name)
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Predecir
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+pred1 = model.predict(images)
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Carga modelo
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+def relu6(x):
+    return K.relu(x, max_value=6)
+
+
+model2 = load_model('Redes/Test2.h5', custom_objects={'relu6': relu6, 'focal_loss': focal_loss})
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Predice resultados
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+print('Empieza predicción')
+
+# with open('persons.csv', 'w') as csvfile:
+#
+#     filewriter = csv.writer(csvfile, delimiter=',')
+#     filewriter.writerow(['ImageID', 'EncodedPixels'])
+rles = []
+
+for cnt, dir in enumerate(addri):
+
+    print(cnt)
+
+    if pred1[cnt] == 0:
+        mask = np.zeros((1024, 1024))
+    else:
+        pred = np.reshape(model2.predict(np.reshape(images[cnt], (1, dim, dim, 1))), (256, 256))
+        # Delets small objects
+        nb_components, output, stats, centroids = cv2.connectedComponentsWithStats((pred*255 > 140).astype(np.uint8) * 255,
+                                                                                   connectivity=8)
+        sizes = stats[1:, -1]
+        nb_components = nb_components - 1
+        min_size = 70
+        y2 = np.zeros(pred.shape, dtype=np.uint8)
+        for i in range(0, nb_components):
+            if sizes[i] >= min_size:
+                y2[output == i + 1] = 255
+
+        nb_components, _, _, _ = cv2.connectedComponentsWithStats(y2, connectivity=8)
+
+        # Codifica máscara
+        mask = (cv2.resize(y2, (1024, 1024)) > 128).astype(np.uint8) * 255
+
+    rles.append(mask2rle(mask, 1024, 1024))
+
+    # filewriter.writerow([name, rle])
+import pandas as pd
+sub_df = pd.DataFrame({'ImageId': ids, 'EncodedPixels': rles})
+sub_df.loc[sub_df.EncodedPixels=='', 'EncodedPixels'] = '-1'
+sub_df.to_csv('submission.csv', index=False)
+sub_df.head()
